d8736e9f2730f2b88f21cbd7635d4616f788e5e7
lxgonzalez <luisgx15@gmail.com>
2026-01-21 11:42:00 -0500

    feat(signature): ✨ add multer configuration for file uploads

--- src/signatures/signatures.module.ts
+++ src/signatures/signatures.module.ts
@@ -7,10 +7,23 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { FilesModule } from 'src/files/files.module';
 import { CreditsModule } from 'src/credits/credits.module';
 
+import { MulterModule } from '@nestjs/platform-express';
+
 @Module({
-  imports: [PrismaModule, ConfigModule, HttpModule, FilesModule, CreditsModule],
+  imports: [
+    PrismaModule,
+    ConfigModule,
+    HttpModule,
+    FilesModule,
+    CreditsModule,
+    MulterModule.register({
+      limits: {
+        fieldSize: 600 * 1024 * 1024,
+      },
+    }),
+  ],
   controllers: [SignaturesController],
   providers: [SignaturesService],
   exports: [SignaturesService],
 })
-export class SignaturesModule {}
+export class SignaturesModule { }
